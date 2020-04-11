800865e80740132459437dbea879977fbdba9819
Emily Ingram <emingram49@gmail.com>
2020-04-11 11:51:33 -0600

    Update test_fitness_sharing_function.py

--- test_fitness_sharing_function.py
+++ test_fitness_sharing_function.py
@@ -3,7 +3,7 @@ from fitness_sharing_function import FitnessSharingFunction
 
 
 def test_init():
-    X = [(0, 0), (0, 1), (1, 0), (1, 1)]
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     y = np.array([0, 1, 1, 0])
 
     EXPECTED_CASES = [X, y]
@@ -15,7 +15,7 @@ def test_init():
     assert fsf._score_matrix == EXPECTED_SCORE_MATRIX
 
 def test_get_semantics():
-    X = [(0, 0), (0, 1), (1, 0), (1, 1)]
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     y = np.array([0, 1, 1, 0])
 
     EXPECTED_SEMANTICS = np.array([0, 0, 0, 1])
@@ -25,7 +25,7 @@ def test_get_semantics():
     assert fsf.get_semantics(lambda a, b: a && b) == EXPECTED_SEMANTICS
 
 def test_get_raw_fitness():
-    X = [(0, 0), (0, 1), (1, 0), (1, 1)]
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     y = np.array([0, 1, 1, 0])
 
     fsf = FitnessSharingFunction(X, y)
@@ -38,6 +38,7 @@ def test_get_raw_fitness():
 
         def get_raw_fitness(self, ind_semantics):
             return np.mean(np.sqrt(self._cases[1]**2 - ind_semantics**2))
+
 
     y_error = np.array([0, 0, 0, 1])
     my_fsf = MyFitnessFunction(X, y)
