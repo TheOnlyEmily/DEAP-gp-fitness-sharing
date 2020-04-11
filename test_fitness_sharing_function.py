957a0531d10e3d0c08c6df54ee08a55d5cbd538f
Emily Ingram <emingram49@gmail.com>
2020-04-11 08:53:09 -0600

    Update test_fitness_sharing_function.py

--- test_fitness_sharing_function.py
+++ test_fitness_sharing_function.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fitness_sharing_function import FitnessSharingFunction
 
 
@@ -5,4 +6,10 @@ def test_init():
     X = [(0, 0), (0, 1), (1, 0), (1, 1)]
     y = [(0,), (1,), (1,), (0,)]
 
+    EXPECTED_CASES = [X, y]
+    EXPECTED_SCORE_MATRIX = np.zeros((1, len(y)))
+
     fsf = FitnessSharingFunction(X, y)
+
+    assert fsf._cases == EXPECTED_CASES
+    assert fsf._score_matrix == EXPECTED_SCORE_MATRIX
