6b2ffb77f552b844f5abb801a6c172c6d63629ce
Emily Ingram <emingram49@gmail.com>
2020-04-14 13:16:01 -0600

    total redesign

--- fitness_sharing_function.py
+++ fitness_sharing_function.py
@@ -5,33 +5,33 @@ class FitnessSharingFunction:
 
     def __init__(self, X, y):
         self._cases = [X, y]
-        self._score_matrix = None
+        self._semantic_matrix = None
 
     def __call__(self, ind):
-        ind_semantics = self.get_semantics(ind)
-        raw_fitness_vector = self.get_raw_fitness(ind_semantics)
-        shared_fitness_vector = self.get_shared_fitness(ind_semantics, raw_fitness_vector)
-        self.register_semantics(ind_indsemantics)
-        return np.sum(raw_fitness_vector / shared_fitness_vector)
+        base_rewards = map(self.get_reward, self.get_semantics(ind))
+        adjusted_reward = sum(self.get_shared_fitness(ind))
+        modified_rewards = map(lambda v: v / adjusted_reward, base_rewards)
+        self.register_semantics(ind)
+        return sum(modified_rewards)
+
+    def get_reward(self, ind):
+        raise NotImplementedError()
 
     def get_semantics(self, ind):
         v_ind = np.vectorize(ind)
         return v_ind(self._cases[0])
 
-    def get_raw_fitness(self, ind_semantics):
-        raise NotImplementedError()
-
-    def get_shared_fitness(self, ind_semantics, error_vector):
-        if self._score_matrix:
-            comparrison_matrix = ind_semantics == self._score_matrix
-            rv = np.sum(comparrision_matrix.astype(np.int64) * error_vector, axis=0)
-            rv[rv == 0] = 1
-            return rv
+    def get_shared_fitness(self, ind):
+        if self._semantic_matrix:
+            weight_vector = np.sum(ind == self._semantic_matrix, axis=0)
+            return weight_vector * self.get_reward(ind)
         else:
-            return np.ones(ind_sematics.size)
+            return np.ones(ind.size) / ind.size
 
-    def register_semantics(self, ind_semantics):
-        if self._score_matrix:
-            self._score_matrix = np.vstack((self._score_matrix, ind_semantics))
+    def register_semantics(self, ind):
+        if self._semantic_matrix:
+            ind_semantics = self.get_semantics(ind)
+            semantic_stack = (self._semantic_matrix, ind_semantics)
+            self._semantic_matrix = np.vstack(semantic_stack)
         else:
-            self._score_matrix = np.array([ind_semantics])
+            self._semantic_matrix = np.array([self.get_semantics(ind)])
