2342f1cceb5862da35cf30ded6912a008b4e0e51
Emily Ingram <emingram49@gmail.com>
2020-04-20 15:44:31 -0600

    Update test_fitness_sharing_function.py

--- test_fitness_sharing_function.py
+++ test_fitness_sharing_function.py
@@ -1,2 +1,14 @@
 import numpy as np
 from fitness_sharing_function import FitnessSharingFunction
+
+
+def test_init():
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    y = np.array([0, 1, 1, 0])
+
+    CASE_LIST = [X, y]
+
+    fsf = FitnessSharingFunction(X, y)
+
+    assert fsf._cases == CASE_LIST
+    assert fsf._semantic_matrix is None
