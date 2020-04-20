d71a3b2c37766a62b4652443639cd7997ae384ff
Emily Ingram <emingram49@gmail.com>
2020-04-20 15:57:21 -0600

    Update test_fitness_sharing_function.py

--- test_fitness_sharing_function.py
+++ test_fitness_sharing_function.py
@@ -22,3 +22,27 @@ def test_get_semantics():
     fsf = FitnessSharingFunction(X, y)
 
     assert fsf.get_semantics(xor) == y
+
+
+class TestGetReward:
+
+    def test_method_not_implemented(self):
+        X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+        y = np.array([0, 1, 1, 0])
+
+        function_semantics = np.array([0, 0, 0, 1])
+
+        fsf = FitnessSharingFunction(X, y)
+
+        with pytest.raises(NotImplementedError):
+            fsf.get_reward(function_semantics)
+
+
+    def test_method_implemented(self):
+        X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+        y = np.array([0, 1, 1, 0])
+
+        class SemDistanceFSF(FitnessSharingFunction):
+
+            def get_reward(self, ind_semantics):
+                return (self._cases[1] - ind_semantics) ** 2
