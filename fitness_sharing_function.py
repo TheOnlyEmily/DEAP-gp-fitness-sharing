18d6a0506bb0639e0eb9a3403762eb797d2f4b52
Emily Ingram <emingram49@gmail.com>
2020-04-20 15:59:37 -0600

    Update fitness_sharing_function.py

--- fitness_sharing_function.py
+++ fitness_sharing_function.py
@@ -8,7 +8,7 @@ class FitnessSharingFunction:
         self._semantic_matrix = None
 
     def __call__(self, ind):
-        base_rewards = map(self.get_reward, self.get_semantics(ind))
+        base_rewards = self.get_reward(self.get_semantics(ind))
         reward_adjust = self.get_shared_fitness(ind)
         adjusted_rewards = map(lambda v: v / reward_adjust, base_rewards)
         self.register_semantics(ind)
