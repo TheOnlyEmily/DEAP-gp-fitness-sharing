6e7a324a9dcd5f62a543711fcd97292d46e55475
Emily Ingram <emingram49@gmail.com>
2020-04-24 18:04:56 -0600

    Update test_fitness_sharing_function.py

--- test_fitness_sharing_function.py
+++ test_fitness_sharing_function.py
@@ -49,7 +49,7 @@ class TestGetReward:
         class SemDistanceFSF(FitnessSharingFunction):
 
             def get_reward(self, ind_semantics):
-                return np.sum((self._cases[1] - ind_semantics) ** 2)
+                return np.mean((self._cases[1] - ind_semantics) ** 2)
 
         fsf = SemDistanceFSF(X, y)
 
@@ -90,7 +90,7 @@ class TestGetSharedFitness:
         class SemDistanceFSF(FitnessSharingFunction):
 
             def get_reward(self, ind_semantics):
-                return np.sum((self._cases[1] - ind_semantics) ** 2)
+                return np.mean((self._cases[1] - ind_semantics) ** 2)
 
 
         fsf = SemDistanceFSF(X, y)
@@ -109,7 +109,7 @@ class TestGetSharedFitness:
         class SemDistanceFSF(FitnessSharingFunction):
 
             def get_reward(self, ind_semantics):
-                return np.sum((self._cases[1] - ind_semantics) ** 2)
+                return np.mean((self._cases[1] - ind_semantics) ** 2)
 
 
         fsf = SemDistanceFSF(X, y)
