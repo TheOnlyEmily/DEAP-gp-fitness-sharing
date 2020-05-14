3e54dc37ff32ac413cc426699ea07bcc35d42192
Emily Ingram <emingram49@gmail.com>
2020-05-14 14:28:37 -0600

    removed unneeded functionality

--- fitness_sharing_function.py
+++ fitness_sharing_function.py
@@ -5,40 +5,24 @@ import numpy as np
 
 class SemanticFitnessSharingFunction:
 
-    def __init__(self, X, y):
-        self._cases = [X, y]
-        self._semantic_matrix = None
+    def __init__(self):
+        self._delta_error_matrix = None
 
-    def __call__(self, ind):
-        base_reward = self.get_fitness(self.get_semantics(ind))
-        reward_adjust = self.get_shared_fitness(ind)
-        self.register_semantics(ind)
-        return base_reward * reward_adjust
+    def __call__(self, delta_error):
+        self.register_error_vector(delta_error)
+        return self.get_shared_fitness(delta_error)
 
-    @property
-    def target_semantics(self):
-        return self._cases[1].copy()
-
-    def get_fitness(self, ind_semantics):
-        raise NotImplementedError()
-
-    def get_semantics(self, ind):
-        map_used = map if self._cases[0].ndim == 1 else starmap
-        return np.array(list(map_used(ind, self._cases[0])))
-
-    def get_shared_fitness(self, ind):
-        if self._semantic_matrix is not None:
-            ind_semantics = self.get_semantics(ind)
-            weight_vector = np.sum(ind_semantics == self._semantic_matrix, axis=0)
-            fit_adjust = np.sum(weight_vector * self.get_fitness(ind_semantics))
+    def get_shared_fitness(self, delta_error):
+        if self._delta_error_matrix is not None:
+            weight_vector = np.sum(delta_error == self._delta_error_matrix, axis=0)
+            fit_adjust = np.sqrt(np.sum((delta_error * weight_vector)**2))
             return fit_adjust if fit_adjust > 0 else 1
         else:
             return 1
 
-    def register_semantics(self, ind):
-        if self._semantic_matrix is not None:
-            ind_semantics = self.get_semantics(ind)
-            semantic_stack = (self._semantic_matrix, ind_semantics)
-            self._semantic_matrix = np.vstack(semantic_stack)
+    def register_error_vector(self, delta_error):
+        if self._delta_error_matrix is not None:
+            delta_error_stack = (self._delta_error_matrix, delta_error)
+            self._delta_error_matrix = np.vstack(delta_error_stack)
         else:
-            self._semantic_matrix = np.array([self.get_semantics(ind)])
+            self._delta_error_matrix = np.array([np.array(detla_error)])
