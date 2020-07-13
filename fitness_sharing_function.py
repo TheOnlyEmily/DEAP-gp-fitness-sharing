7a55e1bdd7e6286b3b131f62a1eb74252195c628
Emily Ingram <emingram49@gmail.com>
2020-07-13 13:17:18 -0600

    add documentation to fitness_sharing_function.py

--- fitness_sharing_function.py
+++ fitness_sharing_function.py
@@ -4,11 +4,29 @@ import numpy as np
 
 
 class SemanticFitnessSharingFunction:
+    """
+    A class used to punish the occurance of simillar program semantics
+    in the course of program evolution. It is intented to be used along
+    side a conventional fitness function in any Genetic Programming 
+    algorithm. 
+    """
 
     def __init__(self):
         self._delta_error_matrix = None
 
     def __call__(self, delta_error):
+        """
+        The main method used for calculating the fitness adjustment
+        factor.
+
+        Args:
+            delta_error (list[Number] | tuple[Number]): The result of 
+            substracting a vector containing the target values and the
+            vector produced by testing the individual.
+
+        Returns: 
+            float: The fitness adjustment factor.
+        """
         delta_error = np.array(delta_error)
         self.register_error_vector(delta_error)
         return self.get_shared_fitness(delta_error)
